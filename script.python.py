0fbc2a43169c597aa29c6934b93544478fba7cb3
Pepepal <p.palacios@pxparchitecture.com>
2020-05-11 09:26:41 -0600

    First Lines of Python
    Class notes for Mimo

---
+++ script.python.py
@@ -0,0 +1,25 @@
+str1 = "Mission "
+str2 = 'accomplished'
+status = str1 + str2
+print(status)
+
+string = "Scene"
+number = 24
+print(string + str(number))
+
+frase1 = " I love"
+frase2 = " Irene"
+frase3 = (frase1 + frase2)
+print(frase3 * 3)
+
+frase4 = "I love Irene so very much"
+print(len(frase4))
+
+novia = ""
+print(frase1 + novia)
+print(type(novia))
+
+character = frase4[2]
+print(character)
+
+
